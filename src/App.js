14f0f28e58a0e615e83f2653e5301891a0c46e3f
unknown <sabn730@gmail.com>
2022-01-01 00:41:19 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='item items-1'>items-1</div>
+        <div className='item items-2'>items-2</div>
+        <div className='item items-3'>items-3</div>
+        <div className='item items-4'>items-4</div>
+        <div className='item items-5'>items-5</div>
+        <div className='item items-6'>items-6</div>
+        <div className='item items-7'>items-7</div>
+        <div className='item items-8'>items-8</div>
+        <div className='item items-9'>items-9</div>
+      </div>
+
+      {/* <div className='container'>
+        <div className='navbar'>Navbar</div>
+        <div className='left-sec'>Left Section</div>
+        <div className='box1'>Box1</div>
+        <div className='box2'>Box2</div>
+        <div className='box3'>Box3</div>
+        <div className='main'>Main Content</div>
+        <div className='footer'>Footer</div>
+      </div> */}
     </div>
   );
 }
